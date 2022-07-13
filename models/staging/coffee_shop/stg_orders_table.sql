2431567974226653eee7de21b425bcf7c06be091
TimdenE <tim.denengelsen@hey.com>
2022-07-13 23:44:43 +0000

    create duplicate of orders for query testing

---
+++ models/staging/coffee_shop/stg_orders_table.sql
@@ -0,0 +1,23 @@
+{{
+  config(
+    materialized = 'table',
+    )
+}}
+
+with source as (
+    select * from {{ source("coffee_shop", "orders") }}
+),
+
+renamed as (
+    select 
+        id as order_id,
+        customer_id,
+        created_at,
+        total,
+        address,
+        state,
+        zip,
+    from source    
+)
+
+select * from renamed
